f33a25c5ce021509d88f415897c557e60a607a57
mauliksinh2003 <144231047+mauliksinh2003@users.noreply.github.com>
2024-02-05 18:50:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import React from 'react';
+import Css from './CSS';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Card from 'react-bootstrap/Card';
+
+function App(){
+  return(
+    <div className='App'>
+      <h1></h1>
+      <Css/>
+    </div>
+  )
+}
+
+
+
+export default App;
---
+++ src/CSS.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Css(){
+    return(
+        <div className="Css">
+            <button style={{backgroundColor : "red"}}>Button 1</button>
+            <button style={{color : "Blue"}}>Button 2</button>
+            <button style={{backgroundColor : "Green"}}>Button 3</button>
+        </div>
+    )
+}
+
+export default Css
